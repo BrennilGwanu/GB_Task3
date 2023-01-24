3139cb9aabc2cffde39441d006c2210ff6b1c328
Snake <per.mortis.non.lates@gmail.com>
2023-01-24 23:22:56 +0500

    comlpleted

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int rem = number % 2;
+if (rem==0)
+{
+    Console.WriteLine("Это число чётное ");
+}
+else 
+{
+    Console.WriteLine("Это число нечётное ");
+}
